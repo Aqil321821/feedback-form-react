eb3872501ee39e29681f5dc884a33c366a136980
Aqil321821 <aqilrao321821@gmail.com>
2024-09-26 23:08:14 +0500

    minor changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,6 +8,7 @@ function AboutPage() {
             <h1>About This Project</h1>
             <p>This is a very Basic Project Of React covering all Basics OF React</p>
             <p>Version 1.0.0</p>
+            <p>Under Supervision Of <span style={{fontStyle: "italic"}}> Sir Muhammed Fareed Soyal</span></p>
         </div>
 
         <Link to='/'>Back To Homepage</Link>
